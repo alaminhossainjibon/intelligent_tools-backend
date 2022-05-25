6cdc9ed24863d87cb777cffeb5ceb3e50f7f52bc
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-26 01:02:43 +0600

    buy api

--- index.js
+++ index.js
@@ -11,7 +11,36 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umtabeb.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('intelligent_tools').collection('products');
+        const buyingCollection = client.db('intelligent_tools').collection('buying');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+        app.post('/buying', async (req, res) => {
+            const buying = req.body;
+            const result = await buyingCollection.insertOne(buying);
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello intelligent tools');
