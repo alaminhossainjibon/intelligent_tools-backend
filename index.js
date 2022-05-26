3fbdc8736e2abdca00cef633fd2aed07ed86d9c0
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-26 13:19:54 +0600

    using jwt token

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
     try {
         await client.connect();
@@ -37,15 +53,22 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         })
 
         // API
-        app.get('/buying', async (req, res) => {
+        app.get('/buying', verifyJWT, async (req, res) => {
             const buyer = req.query.buyer;
-            const query = { buyer: buyer };
-            const buying = await buyingCollection.find(query).toArray();
-            res.send(buying);
+            const decodedEmail = req.decoded.email;
+            if (buyer === decodedEmail) {
+                const query = { buyer: buyer };
+                const buying = await buyingCollection.find(query).toArray();
+                return res.send(buying);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
         app.post('/buying', async (req, res) => {
